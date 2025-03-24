838cbecaec443c19c97283692962da8308910748
farVelasco0128 <business.xandravelasco@gmail.com>
2025-03-24 16:30:04 +0800

    Added additional prompts

--- final_model_dev/app_updated.py
+++ final_model_dev/app_updated.py
@@ -29,8 +29,8 @@ prompt = f"""
 You are a travel planner. Your goal is to recommend whether or not
 a person should go to {location} on a specific month. Take note that if
 it's too crowded, the user might not want to go there. However, if it's
-just rainy and not overcrowded, you can assume that a user is still open
-to a staycation at a hotel or resort. If your answer is no, recommend
+just rainy and not overcrowded, you can assume that a user to other
+activities from the list. If your answer is no, recommend
 another month for the user to go to in order to enjoy their selected 
 activity.
 
@@ -40,10 +40,21 @@ The user wants to go {activity} in {location} during the month of {month}. Based
 - Days Sunny: {selected_data['days_sunny']}
 - Tourist Crowds: {"High" if selected_data['crowded'] else "Low"}
 
-Should I go to {location} on that month? Explain briefly.
+Should I go to {location} on that month? 
+
+Please follow this template. Please offer additional/alternative activities for Activity 2 and 3. Please
+make sure that the activities recommended are from this list only: ["Water Sports", "Hiking", "Staycation", "Nightlife"]
+For the first part, no need to explain.:
+Check Mark/Wrong emoji {activity} \n
+Check Mark/Wrong emoji Activity 2 \n
+Check Mark/Wrong emoji Activity 3
+
+Recommendation: 
+Explain briefly.
+
 """
 
-client = genai.Client(api_key="api_key")
+client = genai.Client(api_key="place_api_key")
 if st.button("Get Recommendation"):
     response = client.models.generate_content(
         model="gemini-1.5-pro",
