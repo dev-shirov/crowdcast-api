22612a3fecdbe47697f2398939cdea85a1bc09df
Matthew Dinglasan <matthew.steven.dinglasan@ing.com>
2025-03-24 18:51:29 +0800

    Added initial draft of LLM integration

--- src/data/load_csv_predictions_to_db.py
+++ src/data/load_csv_predictions_to_db.py
@@ -4,26 +4,63 @@ import sqlite3
 con = sqlite3.connect("./src/data/predictions.db")
 cur = con.cursor()
 cur.execute(
-    "CREATE TABLE IF NOT EXISTS boracay_predictions (id integer , month integer, year integer, days_rained integer, days_cloudy integer, days_sunny integer, tourists integer)"
+    """CREATE TABLE IF NOT EXISTS 
+        predictions 
+            (id integer, 
+            location varchar, 
+            month integer, 
+            days_rained integer, 
+            days_cloudy integer, 
+            days_sunny integer, 
+            tourists integer,
+            crowded varchar,
+            water_sports_score integer,
+            hiking_score integer,
+            staycation_score integer,
+            nightlife_score integer
+            )
+    """
 )
 
-with open("./src/data/boracay_forecast.csv", "r") as f:
+with open("./src/data/llm_dataset.csv", "r") as f:
     reader = csv.DictReader(f)
     to_db = [
         (
             i["id"],
+            i["location"],
             i["month"],
-            i["year"],
             i["days_rained"],
             i["days_cloudy"],
             i["days_sunny"],
             i["tourists"],
+            i["crowded"],
+            i["water_sports_score"],
+            i["hiking_score"],
+            i["staycation_score"],
+            i["nightlife_score"],
         )
         for i in reader
     ]
 
+insert_query = """
+                INSERT OR IGNORE INTO predictions 
+                    (id, 
+                    location, 
+                    month, 
+                    days_rained, 
+                    days_cloudy, 
+                    days_sunny, 
+                    tourists,
+                    crowded,
+                    water_sports_score,
+                    hiking_score,
+                    staycation_score,
+                    nightlife_score
+                    ) 
+                VALUES (?, ?, ?, ? ,? ,?, ?, ?, ?, ?, ? , ?);
+                """
 cur.executemany(
-    "INSERT OR IGNORE INTO boracay_predictions (id, month, year, days_rained, days_cloudy, days_sunny, tourists) VALUES (?, ?, ?, ? ,? ,?, ?);",
+    insert_query,
     to_db,
 )
 con.commit()
--- src/models/schemas.py
+++ src/models/schemas.py
@@ -1,25 +1,69 @@
-from typing import Dict
+from typing import Optional, Dict
 
-from sqlalchemy import Column, Integer
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import Session
+import pandas as pd
 
 from .database import Base
+
+from .models import RecommendationModel
+
+from src.chains.crowdcast_recommendation import generate_vacation_idea_chain
 
 
 
 class Predictions(Base):
     """tickets table definition"""
 
-    __tablename__ = "boracay_predictions"
+    __tablename__ = "predictions"
 
     id = Column(Integer, primary_key=True)
+    location = Column(String)
     month = Column(Integer)
-    year = Column(Integer)
     days_rained = Column(Integer)
     days_cloudy = Column(Integer)
     days_sunny = Column(Integer)
     tourists = Column(Integer)
+    crowded = Column(String)
+    water_sports_score = Column(Integer)
+    hiking_score = Column(Integer)
+    staycation_score = Column(Integer)
+    nightlife_score = Column(Integer)
+
+def query_predictions(month: Optional[int], location: Optional[str], db: Session) -> Dict[str, list[Predictions]]:
+    
+    if location and month:
+        return  (
+            {"predictions": db.query(Predictions).filter(Predictions.location == location, Predictions.month == month).all()}
+        )
+    elif location is None and month:
+         return  (
+            {"predictions": db.query(Predictions).filter(Predictions.month == month).all()}
+        )
+    elif location and month is None:
+         return  (
+            {"predictions": db.query(Predictions).filter(Predictions.location == location).all()}
+        )
+    else: 
+         return  (
+            {"predictions": db.query(Predictions).all()}
+        )
+    
+def query_recommendation(month: int, location: str, activity: str, db: Session) -> Optional[RecommendationModel]: 
+
+    input_dict = query_predictions(month, location, db)["predictions"][0].__dict__
+
+    month = input_dict["month"]
+    location = input_dict["location"]
+    days_rained = input_dict["days_rained"]
+    days_cloudy = input_dict["days_cloudy"]
+    days_sunny = input_dict["days_sunny"]
+    crowded = "High" if input_dict["crowded"] else "Low" 
+
+    recommendation_str = generate_vacation_idea_chain(month, location, days_rained,days_cloudy, days_sunny, crowded, activity)
+
+    return RecommendationModel(recommendation=recommendation_str)
 
 
-def query_predictions(db: Session) -> Dict[str, list[Predictions]]:
-    return {"predictions": db.query(Predictions).all()}
+
+
